8edc454e616bf236cfb51da34dddc100dfc47fee
eduardo-salazar <esalazar922@gmail.com>
2017-01-03 02:21:08 +0800

    Adde conflix with names

--- app.py
+++ app.py
@@ -22,15 +22,16 @@ def calc_user_interaction():
     return jsonify(interaction)
 
 @app.route('/getTPVideos', methods=['POST'])
-def calc_user_interaction():
+def getTPVideos():
     user_videos = request.get_json()
     csv_file = pandas.read_csv("data/clustersByWordLevel.csv",header=None)
     helper = Helper(user_videos,csv_file)
     interaction = helper.calc_user_interaction()
     print(interaction)
 
-    # Get cluster of TP 
-    return jsonify(interaction)
+    result = { "cluster": 0 , "videos": [1231,34235,12335,5345345,123123]}
+    # Get cluster of TP
+    return jsonify(result)
 
 
 if __name__ == '__main__':
